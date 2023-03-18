f434a33b82550ad4d258cc3c2bc25bb216ab7873
chris-santiago <cjsantiago@gatech.edu>
2023-03-17 21:54:01 -0400

    remove hellos

--- {{cookiecutter.project_slug}}/tests/test_hello_world.py
+++
@@ -1,7 +0,0 @@
-from {{cookiecutter.project_slug}}.hello_world import hello_world
-
-
-def test_hello_world():
-    actual = hello_world()
-    expected = 'Hello World.'
-    assert actual == expected
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/hello_world.py
+++
@@ -1,6 +0,0 @@
-"""Hello world module."""
-
-
-def hello_world(msg: str = "Hello World.") -> str:
-    """Return hello world."""
-    return msg
